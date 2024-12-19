5dd2886c4e7586e3e2f4d01b817eb74de300493c
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-12-18 18:33:43 -0700

    made wofi look better in hyprland

--- wofi/.config/wofi/scripts/runmenu.sh
+++ wofi/.config/wofi/scripts/runmenu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wofi_running=$(pgrep wofi)
 if [ -z "$wofi_running" ]; then
-		wofi -I --show=drun
+		WOFI_LOG_LEVEL=debug wofi -I --show=drun > ~/wofi_debug.log 2>&1
 else
     killall wofi
 fi
