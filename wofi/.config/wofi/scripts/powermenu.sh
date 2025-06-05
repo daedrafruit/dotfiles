17a489cf93642cedf2b95ea831ed28ac47b0de7a
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2025-06-04 19:06:55 -0600

    cleaned up, improved some keybinds and interactions

--- wofi/.config/wofi/scripts/powermenu.sh
+++ wofi/.config/wofi/scripts/powermenu.sh
@@ -1,40 +1,33 @@
 #!/bin/sh
+# use zero-width spaces to manipulate alphabetical sorting
+Z=$'\u200B'
+options=$( echo -e "${Z}${Z}${Z}${Z}${Z}${Z}  Tasks\n${Z}${Z}${Z}${Z}${Z}󰋊  Hibernate\n${Z}${Z}${Z}${Z}  Sleep\n${Z}${Z}${Z}  Reboot\n${Z}${Z}  Logout\n${Z}  Shutdown")
 
-wofi_running=$(pgrep wofi)
-if [ -z "$wofi_running" ]; then
+op=$(echo -e "$options" | wofi -i --dmenu --sort-order alphabetical --width 300 --height 275)
 
-	# use zero-width spaces to manipulate alphabetical sorting
-	Z=$'\u200B'
-	options=$( echo -e "${Z}${Z}${Z}${Z}${Z}${Z}  Tasks\n${Z}${Z}${Z}${Z}${Z}󰋊  Hibernate\n${Z}${Z}${Z}${Z}  Sleep\n${Z}${Z}${Z}  Reboot\n${Z}${Z}  Logout\n${Z}  Shutdown")
+op=$(echo "$op" | awk '{print tolower($2)}')
 
-	op=$(echo -e "$options" | wofi -i --dmenu --sort-order alphabetical --width 300 --height 275)
+case $op in
+	"tasks")
+		hyprctl dispatch exec "[float;size 50% 75%;]" ~/dotfiles/wofi/.config/wofi/scripts/btp.sh
+		;;
+	"hibernate")
+		systemctl hibernate
+		;;
+	"shutdown")
+		systemctl poweroff
+		;;
+	"reboot")
+		systemctl reboot
+		;;
+	"sleep")
+		systemctl suspend
+		;;
+	"logout")
+		hyprctl dispatch exit
+		;;
+esac
 
-	op=$(echo "$op" | awk '{print tolower($2)}')
-
-	case $op in
-		"tasks")
-			hyprctl dispatch exec "[float;size 50% 75%;]" ~/dotfiles/wofi/.config/wofi/scripts/btp.sh
-			;;
-		"hibernate")
-			systemctl hibernate
-			;;
-		"shutdown")
-			systemctl poweroff
-			;;
-		"reboot")
-			systemctl reboot
-			;;
-		"sleep")
-			systemctl suspend
-			;;
-		"logout")
-			hyprctl dispatch exit
-			;;
-	esac
-
-else
-    killall wofi
-fi
 #  Lock\n
   #"lock")
   #  swaylock -f -c 000000
--- wofi/.config/wofi/scripts/runmenu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wofi_running=$(pgrep wofi)
-if [ -z "$wofi_running" ]; then
-		wofi -I --show=drun
-else
-    killall wofi
-fi
