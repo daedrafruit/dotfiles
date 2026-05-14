0b627890398dd300118f7a34fc1edb6ead69655f
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-05-14 01:19:04 -0700

    added themes

---
+++ kde/.local/share/plasma/look-and-feel/catppuccin/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,395 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1920x1080": "",
+                    "ItemGeometries-2560x1440": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "888",
+                    "DialogWidth": "1397"
+                },
+                "/General": {
+                    "changedPositions": "{}",
+                    "lastResolution": "1920x1080",
+                    "positions": "{\"1920x1080\":[\"1\",\"17\"],\"2560x1440\":[\"1\",\"23\",\"desktop:/brute.py\",\"0\",\"6\",\"desktop:/secretfile.txt.gz\",\"0\",\"7\",\"desktop:/Archive.zip\",\"0\",\"4\",\"desktop:/demo_time_space_diagram_full.png\",\"0\",\"11\",\"desktop:/plot.py\",\"0\",\"5\",\"desktop:/addons\",\"0\",\"16\",\"desktop:/TI-Nspire Student Software.desktop\",\"0\",\"0\",\"desktop:/TI-Nspire Student Software.lnk\",\"0\",\"2\",\"desktop:/songs.txt\",\"0\",\"14\",\"desktop:/tmp\",\"0\",\"13\",\"desktop:/jdtls.sh\",\"0\",\"15\",\"desktop:/lsp_signiture.lua\",\"0\",\"9\",\"desktop:/manga-chat-gpt.py\",\"0\",\"10\",\"desktop:/bms-song-comp.py\",\"0\",\"8\",\"desktop:/jdtls.lua\",\"0\",\"17\",\"desktop:/ogg.sh\",\"0\",\"12\",\"desktop:/86Box-Linux-x86_64-b7777.AppImage\",\"0\",\"1\",\"desktop:/demo_time_space_diagram_split.png\",\"0\",\"3\"]}",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///home/daedr/Pictures/wallpapers/wallhaven-n6qwv6.png",
+                    "SlidePaths": "/usr/share/wallpapers/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        },
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1920x1080": "",
+                    "ItemGeometries-2560x1440": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "1",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/General": {
+                    "positions": "{\"1920x1080\":[],\"2560x1440\":[]}"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///home/daedr/Pictures/wallpapers/wallhaven-1pj829.jpg",
+                    "SlidePaths": "/usr/share/wallpapers/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "515",
+                            "popupWidth": "683"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "systemFavorites": "suspend\\,hibernate\\,reboot\\,shutdown"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        },
+                        "/General": {
+                            "highlightWindows": "true",
+                            "launchers": "applications:org.kde.konsole.desktop,applications:firefox.desktop,applications:org.kde.dolphin.desktop,applications:org.kde.kate.desktop,applications:steam.desktop,file:///usr/share/applications/discord.desktop,applications:obsidian.desktop,applications:feishin.desktop",
+                            "wheelEnabled": "AdjustVolume"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "451",
+                            "popupWidth": "810"
+                        },
+                        "/Appearance": {
+                            "enabledCalendarPlugins": "alternatecalendar,astronomicalevents",
+                            "fontWeight": "400"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2.4444444444444446,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 106.66666666666667,
+            "minimumLength": 106.66666666666667,
+            "offset": 0
+        },
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "515",
+                            "popupWidth": "655"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        },
+                        "/General": {
+                            "highlightWindows": "true",
+                            "launchers": "applications:org.kde.konsole.desktop,applications:firefox.desktop,applications:org.kde.dolphin.desktop,applications:org.kde.kate.desktop,applications:steam.desktop,applications:discord.desktop,applications:obsidian.desktop,applications:feishin.desktop",
+                            "wheelEnabled": "AdjustVolume"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "CurrentPreset": "org.kde.plasma.systemmonitor",
+                            "popupHeight": "504",
+                            "popupWidth": "300"
+                        },
+                        "/Appearance": {
+                            "chartFace": "org.kde.ksysguard.barchart",
+                            "title": "Individual Core Usage"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/SensorColors": {
+                            "cpu/cpu.*/usage": "166,227,161",
+                            "cpu/cpu0/usage": "166,227,161",
+                            "cpu/cpu1/usage": "161,227,181",
+                            "cpu/cpu10/usage": "227,161,183",
+                            "cpu/cpu11/usage": "227,164,161",
+                            "cpu/cpu12/usage": "227,189,161",
+                            "cpu/cpu13/usage": "227,214,161",
+                            "cpu/cpu14/usage": "216,227,161",
+                            "cpu/cpu15/usage": "191,227,161",
+                            "cpu/cpu2/usage": "161,227,205",
+                            "cpu/cpu3/usage": "161,224,227",
+                            "cpu/cpu4/usage": "161,199,227",
+                            "cpu/cpu5/usage": "161,174,227",
+                            "cpu/cpu6/usage": "172,161,227",
+                            "cpu/cpu7/usage": "197,161,227",
+                            "cpu/cpu8/usage": "222,161,227",
+                            "cpu/cpu9/usage": "227,161,207"
+                        },
+                        "/Sensors": {
+                            "highPrioritySensorIds": "[\"cpu/cpu.*/usage\"]",
+                            "totalSensors": "[\"cpu/all/usage\"]"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemmonitor.cpucore"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "CurrentPreset": "org.kde.plasma.systemmonitor",
+                            "popupHeight": "257",
+                            "popupWidth": "215"
+                        },
+                        "/Appearance": {
+                            "chartFace": "org.kde.ksysguard.piechart",
+                            "title": "Total CPU Use"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/SensorColors": {
+                            "cpu/all/averageFrequency": "227,161,210",
+                            "cpu/all/usage": "166,227,161",
+                            "cpu/all/wait": "227,224,161"
+                        },
+                        "/Sensors": {
+                            "highPrioritySensorIds": "[\"cpu/all/usage\"]",
+                            "lowPrioritySensorIds": "[\"cpu/all/maximumTemperature\",\"cpu/all/averageFrequency\",\"cpu/all/wait\"]",
+                            "totalSensors": "[\"cpu/all/averageTemperature\",\"cpu/all/usage\"]"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemmonitor.cpu"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "CurrentPreset": "org.kde.plasma.systemmonitor",
+                            "popupHeight": "279",
+                            "popupWidth": "222"
+                        },
+                        "/Appearance": {
+                            "chartFace": "org.kde.ksysguard.piechart",
+                            "title": "Total GPU Use"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/SensorColors": {
+                            "cpu/all/usage": "166,227,161",
+                            "gpu/gpu1/usage": "168,227,161"
+                        },
+                        "/SensorLabels": {
+                            "gpu/gpu1/power": "GPU Power",
+                            "gpu/gpu1/temperature": "GPU Temperature",
+                            "gpu/gpu1/totalVram": "GPU Total Video Memory",
+                            "gpu/gpu1/usage": "GPU Usage",
+                            "gpu/gpu1/usedVram": "GPU Video Memory Used"
+                        },
+                        "/Sensors": {
+                            "highPrioritySensorIds": "[\"gpu/gpu1/usage\"]",
+                            "lowPrioritySensorIds": "[\"gpu/gpu1/power\",\"gpu/gpu1/usedVram\",\"gpu/gpu1/totalVram\"]",
+                            "totalSensors": "[\"gpu/gpu1/temperature\",\"gpu/gpu1/usage\"]"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemmonitor.cpu"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "CurrentPreset": "org.kde.plasma.systemmonitor",
+                            "popupHeight": "235",
+                            "popupWidth": "238"
+                        },
+                        "/Appearance": {
+                            "chartFace": "org.kde.ksysguard.piechart",
+                            "title": "Memory Usage"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/SensorColors": {
+                            "memory/physical/used": "166,227,161"
+                        },
+                        "/Sensors": {
+                            "highPrioritySensorIds": "[\"memory/physical/used\"]",
+                            "lowPrioritySensorIds": "[\"memory/physical/total\"]",
+                            "totalSensors": "[\"memory/physical/usedPercent\"]"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemmonitor.memory"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "CurrentPreset": "org.kde.plasma.systemmonitor",
+                            "popupHeight": "195",
+                            "popupWidth": "306"
+                        },
+                        "/Appearance": {
+                            "chartFace": "org.kde.ksysguard.linechart",
+                            "title": "Network Speed"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/SensorColors": {
+                            "network/all/download": "166,227,161",
+                            "network/all/upload": "222,161,227"
+                        },
+                        "/Sensors": {
+                            "highPrioritySensorIds": "[\"network/all/download\",\"network/all/upload\"]",
+                            "lowPrioritySensorIds": "[\"network/enp9s0/ipv4address\"]"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemmonitor.net"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "CurrentPreset": "org.kde.plasma.systemmonitor",
+                            "popupHeight": "226",
+                            "popupWidth": "175"
+                        },
+                        "/Appearance": {
+                            "chartFace": "org.kde.ksysguard.linechart",
+                            "title": "Hard Disk Activity"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/SensorColors": {
+                            "cpu/cpu0/name": "217,227,161",
+                            "disk/all/read": "222,161,227",
+                            "disk/all/write": "166,227,161"
+                        },
+                        "/SensorLabels": {
+                            "disk/2b6292aa-6116-4ead-9e96-cd502090b69c/free": "Free",
+                            "disk/2b6292aa-6116-4ead-9e96-cd502090b69c/usedPercent": "Used"
+                        },
+                        "/Sensors": {
+                            "highPrioritySensorIds": "[\"disk/all/write\",\"disk/all/read\"]",
+                            "lowPrioritySensorIds": "[\"disk/2b6292aa-6116-4ead-9e96-cd502090b69c/free\",\"disk/2b6292aa-6116-4ead-9e96-cd502090b69c/usedPercent\"]"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemmonitor.diskactivity"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "451",
+                            "popupWidth": "560"
+                        },
+                        "/Appearance": {
+                            "fontFamily": "Hack",
+                            "fontWeight": "400"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "1",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2.4444444444444446,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 142.22222222222223,
+            "minimumLength": 142.22222222222223,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
